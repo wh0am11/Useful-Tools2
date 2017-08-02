3f087eaf50f6845028b8dd3558d79b3ef42bd20b
wh0am11 <wh0am1terminal@gmail.com>
2017-08-01 23:18:22 -0500

    added installation and uninstallation files

---
+++ ping2/uninstall_ping2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+#################
+#
+# Author: wh0am1
+# Description: Uninstalls ping2
+# Date: 8-1-17
+#
+#################
+
+sudo rm "/usr/local/bin/ping2"
+sudo rm "/usr/local/sbin/ping2"
+
+echo "[*] ping2 successfully uninstalled"
