4a65ec679c467e54cd00ec9d4b06418d7d7b490f
wh0am11 <wh0am1terminal@gmail.com>
2017-08-01 22:52:56 -0500

    quotations not required for arguments for ping or ping6

--- ping2/ping2.c
+++ ping2/ping2.c
@@ -17,20 +17,22 @@
 
 int main(int argc,char* argv[]) {
 
-if (argc != 3) { // if invalid number of arguments provided
+if (argc < 3) { // if invalid number of arguments provided
 
-fprintf(stderr,"ping2: Exactly 2 arguments required, Usage: ./ping2 [IP version] [arguments for ping or ping6]\n");
+fprintf(stderr,"ping2: Exactly 2 or more arguments required, Usage: ./ping2 [IP version] [arguments for ping or ping6]\n");
 return -1;
 
 }
 
 char version[2]; // version number
-char args[strlen(argv[2])+1]; // arguments 
-char* full_command = malloc(strlen(argv[2])+7); // allocate memory for full command
+char* args = malloc(80000);
+char* full_command = malloc(80000); // allocate memory for full command
 
-if (full_command == NULL) {
+if (full_command == NULL || args == NULL) {
 
 perror("");
+if (full_command != NULL) free(full_command);
+else free(args);
 return -1;
 
 }
@@ -38,26 +40,35 @@ return -1;
 /* Copy arguments */
 
 strncpy(version,argv[1],2); 
-strncpy(args,argv[2],strlen(argv[2]));
+strncpy(args,argv[2],80000);
+
+for (unsigned int i = 3; i < argc; i++) {
+
+strncat(args," ",1);
+strncat(args,argv[i],80000);
+
+}
+
 
 /* Check version number */
 
-if (!strcmp(version,"4")) {
+if (!strcmp(version,"4")) { // IPv4
 
 strncpy(full_command,"ping ",5);
-strncat(full_command,args,strlen(argv[2]));
+strncat(full_command,args,80000);
 
 }
-else if (!strcmp(version,"6")) {
+else if (!strcmp(version,"6")) { // IPv6
 
 strncpy(full_command,"ping6 ",6);
-strncat(full_command,args,strlen(argv[2]));
+strncat(full_command,args,80000);
 
 }
-else {
+else { // invalid version
 
 fprintf(stderr,"ping2: invalid IP version provided\n");
 free(full_command); // free memory
+free(args);
 return -1;
 
 }
@@ -65,6 +76,7 @@ return -1;
 system(full_command); // execute on system
 
 free(full_command); // free memory
+free(args);
 
 return 0;
 
