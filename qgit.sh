8141e2c111e1c08539d4227ac0c21253f91d984d
wh0am11 <wh0am1terminal@gmail.com>
2017-09-15 21:35:13 -0500

    quickly commit to git repos

---
+++ qgit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+###################
+#
+# Author: wh0am1
+# Description: Commits files in the current directory
+# Date: 9-15-17
+#
+####################
+
+
+if [[ "$1" == "" ]]
+then
+
+	echo "Error: Couldn't commit file" 2>&1
+
+else 
+
+	git add . 
+	git commit -m "$1"
+	git push origin master
+
+fi
