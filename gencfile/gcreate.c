0e69cdd2e0926c66ce39e200a7852959c5d11438
wh0am11 <wh0am1terminal@gmail.com>
2017-11-27 14:38:43 -0600

    redid gencfile

--- gencfile/gcreate.c
+++ gencfile/gcreate.c
@@ -1,178 +1,74 @@
+
+
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <pwd.h>
-#include <time.h>
+#include <errno.h>
 
-/*
-######################
-#
-# Author: wh0am1
-# Description: Creates C Files automatically without me having to write descriptions like this
-# Date: 9-18-17
-#
-#####################
-*/
+#define MAX_DATA 5000
 
-int check_empty(char* str);
-int write_data(char* name, char* author, char* desc, char* date, int no_main);
+void write_h_file(FILE *fp) {
 
-int main(int argc,char* argv[]) {
+	const char *data = "#ifdef __something__\n#define __something__\n\n#include <stdio.h>\n\n#endif\n";
 
-	char name[1000] = "";
-	char author[100] = "";
-	char description[1000] = "";
-	char date[100] = ""; 
-	int y = 1;
+	if (fwrite(data, strlen(data), 1, fp) < 0) {
 
-	if (argc < 2) {
-
-		fprintf(stderr,"Usage: gcreate [name] [author] [description] [date]\n");
-		return -1;
-	}
-	else if (argc == 2) {
-
-		strncpy(name,argv[1],1000);
-	}
-	else if (argc == 3) {
-		
-		strncpy(name,argv[1],1000);
-		strncpy(author,argv[2],100);
-	}
-	else if (argc == 4)  {
-
-		strncpy(name,argv[1],1000);
-		strncpy(author,argv[2],100);
-		strncpy(description,argv[3],1000);
-	}
-	else if (argc == 5) {
-
-		strncpy(name,argv[1],1000);
-                strncpy(author,argv[2],100);
-                strncpy(description,argv[3],1000);
-		strncpy(date,argv[4],100);
-
-	}
-	else if (argc == 6) {
-
-		strncpy(name,argv[1],1000);
-		strncpy(author,argv[2],100);
-		strncpy(description,argv[3],1000);
-		strncpy(date,argv[4],100);
-		if (!strcmp(argv[5],"0")) y = 0;		
-	}
-	else {
-
-		fprintf(stderr,"Too many arguments\n");
+		perror("Unable to write to file");
+		return;
 	}
 
-	write_data(name, author, description, date, y);
-
-	return 0;
 
 }
 
-int check_empty(char* str)  {
+void write_c_file(FILE *fp) {
 
-	/* Check if string is empty */
+	const char *data = "\n\n#include <stdio.h>\n\n\n\nint main(int argc, char *argv[]) {\n\n\nreturn 0;\n\n}";
 
-	if (!strcmp(str,"")) return 1;
-	return 0;
+	if (fwrite(data, strlen(data), 1, fp) < 0) {
+
+		perror("Unable to write to file");
+		return;
+	}
 
 }
 
-int write_data(char* name, char* author, char* desc, char* date, int no_main) {
+int main(int argc, char *argv[]) {
 
-	/* Write data into file */
+	if (argc != 2)  {
 
-	FILE* fp = NULL;
-	char str[BUFSIZ];
-	char namext[BUFSIZ];
-
-	if (strlen(name) < BUFSIZ) {
-
-		strncpy(namext,name,strlen(name));
-
-		if (!no_main)
-			strncat(namext,".h",2);
-		else
-			strncat(namext,".c",2);
-
-	}
-	else {
-		fprintf(stderr,"File name is too large\n");
+		fprintf(stderr, "\n\ngcreate: Exactly 1 argument required.\nUsage: gcreate [filename]\n\n");
 		return -1;
 	}
 
-	fp = fopen(namext,"w");
+	char *filename = malloc(sizeof(char) * MAX_DATA + 1);
+	if (filename == NULL) {
 
-	if (!fp) return -1;
-
-	
-	strncpy(str,"#include <stdio.h>\n\n\n/*\n##############\n#\n",BUFSIZ);
-
-	if (check_empty(author) == 0) {
-	
-		strncat(str,"# Author: ",BUFSIZ);
-		strncat(str,author,BUFSIZ);
-		strncat(str,"\n",1);
-	}
-	else {
-
-		struct passwd* p = getpwuid(getuid());
-
-		strncat(str,"# Author: ",BUFSIZ);
-		strncat(str,p->pw_name,BUFSIZ);
-		strncat(str,"\n",1);
-
+		perror("gcreate");
+		return -1;
 	}
 
-	if (check_empty(desc) == 0) {
+	strncpy(filename, argv[1], MAX_DATA + 1);
+	FILE *f = fopen(filename, "w");
 
-		strncat(str,"# Description: ",BUFSIZ);
-		strncat(str,desc,BUFSIZ);
-		strncat(str,"\n",1);
+	if (f == NULL) {
 
-	}
-	else {
-
-		strncat(str,"# Description: A generic description\n",BUFSIZ);
-
+		free(filename);
+		fprintf(stderr, "\n\ngcreate: Couldn't open %s for writing.\nReason: %s\n\n\n", filename, strerror(errno));
+		return -1;
 	}
 
-	if (check_empty(date) == 0) {
+	char *ext = strtok(filename, ".");
+	if (ext) ext = strtok(NULL, ".");
 
-		strncat(str,"# Date: ",BUFSIZ);
-		strncat(str,date,BUFSIZ);
-		strncat(str,"\n",1);
+	if (!ext || strcmp(ext, "c") == 0)
+			write_c_file(f);
+	else 
+			write_h_file(f);
 
-	}
-	else {
-		
-		time_t d = time(NULL);
-		char* date = ctime(&d);
 
-		strncat(str,"# Date: ",BUFSIZ);
-
-		char* line = strtok(date," ");
-
-		line = strtok(NULL," ");
-		strncat(str,line,BUFSIZ);
-		strncat(str," ",1);
-		line = strtok(NULL," ");
-		strncat(str,line,BUFSIZ);
-		strncat(str," ",1);
-		line = strtok(NULL," ");
-		line = strtok(NULL," ");
-		strncat(str,line,BUFSIZ);
-
-	}
-	strncat(str,"#\n##############\n*/\n",BUFSIZ);
-	if (no_main) strncat(str,"\n\nint main(int argc,char* argv[]) {\n\n\n\t/* Auto-generated C File */\n\n\treturn 0;\n\n\n}\n",BUFSIZ);
-
-	fwrite(str,1,strlen(str),fp);
-
-	fclose(fp);
+	if (filename) free(filename);
+	if (f) fclose(f);
 
 	return 0;
 
