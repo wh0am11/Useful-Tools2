804647518d6fefb47f07b47f10aa8c7a2146e9bb
wh0am11 <wh0am1terminal@gmail.com>
2018-01-01 17:35:47 -0600

    Now directories can be moved with --move-dirs

--- mvf.sh
+++ mvf.sh
@@ -10,8 +10,6 @@
 #
 ############################
 
-
-
 if [[ "$1" == "" ]]
 then
 	echo "Error argument must be provided"
@@ -25,9 +23,14 @@ fi
 
 for i in $(ls)
 do
+
 	if [[ -f $i ]]
 	then
 		mv "$i" "$1"
+
+	elif [[ "$2" == "--move-dirs" && -d $i ]]
+	then
+		mv "$i"	"$1" 2>>/dev/null # A lazy but efficient way to get rid of the "cannot move subdirectory into itself" error
 	fi
 
 done
